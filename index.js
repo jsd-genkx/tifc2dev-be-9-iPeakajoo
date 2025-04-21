d2ffa5ac836ea17c2f7fc586913c2741bdd36f6d
iPeakajoo <peakajoo@gmail.com>
2025-04-21 15:19:45 +0700

    Update package.json to use Express 5.1.0 and modify project metadata

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ app.use(express.json());
 
 // Simulated data for API
 const books = [
-  { id: 1, title: "1984", author: "George Orwell", genre: "Dystopian" },
+  { id: 1,
+    title: "1984",
+    author: "George Orwell",
+    genre: "Dystopian"
+  },
   {
     id: 2,
     title: "To Kill a Mockingbird",
     author: "Harper Lee",
+    genre: "Fiction",
+  },
+  {
+    id: 3,
+    title: "Testing book3",
+    author: "Teeramaet",
     genre: "Fiction",
   },
 ];
@@ -19,28 +29,56 @@ const books = [
 app.get("/books", (req, res, next) => {
   setTimeout(() => {
     const { genre } = req.query;
-    //TODO: ADD CODE HERE ⬇️ to Filter books by genre.
-    const filteredBooks = books.filter((book) => book.genre.includes(genre));
-    //TODO: ADD CODE HERE ⬇️
+      if(genre) {
+        const filteredBooks = books.filter((book) => book.genre.includes(genre));
+        res.status(200)
+        res.json(filteredBooks)
+      } else {
+        res.json(books)
+      }
   }, 1000); // Simulate a 1-second delay
 });
 
 // GET specific book by ID with async/await
 app.get("/books/:id", async (req, res, next) => {
-  const book = await new Promise((resolve, reject) => {
+  try {
+    const book = await new Promise((resolve, reject) => {
     setTimeout(() => {
-      const foundBook = books.find((b) => b.id === parseInt(req.params.id, 10));
+        const foundBook = books.find((b) => b.id === parseInt(req.params.id, 10));
       if (foundBook) {
         resolve(foundBook);
       } else {
-        //TODO: ADD CODE to reject the promise
+        const err = new Error("Book not found")
+        err.status = 404;
+        reject(err);
       }
     }, 1000); // Simulate a 1-second delay
   });
-  //TODO: ADD CODE HERE ⬇️
+
+    res.json(book);
+    } catch(err) {
+      console.log(err)
+      next(err)
+    }
+
 });
 
-//TODO: ADD CODE HERE ⬇️
+app.use((err,req,res,next) => {
+  const status = err.status || 500;
+  const response = {
+    message: err.message || "internal server error",
+    status: status
+  };
+
+  if (process.env.NODE_ENV === "development") {
+    response.stack = err.stack;
+  }
+
+  console.error(err.stack)
+  res.status(status).json(response)
+})
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
